8998c5a7235f6611eb23c06142d8d05ecdc9076f
Rakibul Hasan <Rakibul Hasan@RAKIB-ERP>
2023-01-06 11:45:58 +0600

    Add project files.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int CategoryOrder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
