3fdbbcac776612a7ddf9e4ce35d0f806a944c652
Rakibul Hasan <rakibulhasansanto25@gmail.com>
2023-01-18 23:44:07 +0600

    Add The Edite Option Which Help Us To Edite The Category.

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyBookWeb.Data;
 using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BulkyBookWeb.Controllers
 {
@@ -27,6 +28,47 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name == obj.CategoryOrder.ToString())
+            {
+                // ModelState.AddModelError("CustomError", "The Display Can't exactly Match The Name!");
+                ModelState.AddModelError("Name", "The Display Can't Exactly Match The Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //For Edite 
+        public IActionResult Edite(int? id) 
+        {
+            if(id == null || id==0) 
+            {
+                return NotFound();
+            }
+            //Three Typpe of retrive the Category
+            var categoryFromDB = _db.categories.Find(id);
+           // var categoryFromDBFirst = _db.categories.FirstOrDefault(u=>u.Id==id);
+           // var categoryFromDBSingle = _db.categories.SingleOrDefault(u => u.Id == id); 
+
+            if(categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edite(Category obj)
+        {
+            if (obj.Name == obj.CategoryOrder.ToString())
+            {
+                // ModelState.AddModelError("CustomError", "The Display Can't exactly Match The Name!");
+                ModelState.AddModelError("Name", "The Display Can't Exactly Match The Name");
+            }
             if (ModelState.IsValid)
             {
                 _db.categories.Add(obj);
--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -8,6 +9,8 @@ namespace BulkyBookWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName ("Dispaly Order")]
+        [Range (1,100,ErrorMessage ="Display Order Must Be to 100 only")]
         public int CategoryOrder { get; set; }
         public DateTime CreateDateTime { get; set; } = DateTime.Now;
     }
